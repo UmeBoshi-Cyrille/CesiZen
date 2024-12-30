346afbbac5d375296f4142645700ce72fb20f0c5
Cyrille Umehara <Umehara_Cyrille@outlook.com>
2024-12-30 17:40:52 +0100

    (chore): Register service added and prepared

--- CesiZen.Domain/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CesiZen.Domain
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ CesiZen.Infrastructure/InfrastructureRegisterService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CesiZen.Infrastructure;
+
+public static class InfrastructureRegisterService
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+    {
+
+        return services;
+    }
+}
