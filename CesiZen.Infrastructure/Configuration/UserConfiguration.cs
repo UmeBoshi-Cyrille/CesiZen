b164e3c73d2ba93e189ae103c40c65028793cc60
Cyrille Umehara <Umehara_Cyrille@outlook.com>
2025-01-07 22:14:29 +0100

    feat(Auth): prepared context and configuration for connecting to DB and related User to Db

---
+++ CesiZen.Infrastructure/Configuration/UserConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using CesiZen.Domain.Datamodel;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MongoDB.EntityFrameworkCore.Extensions;
+
+namespace CesiZen.Infrastructure.Configuration;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToCollection("users");
+
+        builder.HasKey(u => u.Id);
+
+        builder.Property(u => u.Username).IsRequired();
+        builder.Property(u => u.Email).IsRequired();
+        builder.Property(u => u.PasswordHash).IsRequired();
+        builder.Property(u => u.Salt).IsRequired();
+        builder.Property(u => u.CreatedAt).IsRequired();
+        builder.Property(u => u.IsActive).IsRequired();
+        builder.Property(u => u.Role);
+
+        builder.HasIndex(u => u.Email).IsUnique();
+        builder.HasIndex(u => u.Username);
+
+    }
+}
---
+++ CesiZen.Infrastructure/DatabaseContext/MongoDbContext.cs
@@ -0,0 +1,22 @@
+﻿using CesiZen.Domain.Datamodel;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace CesiZen.Infrastructure.DatabaseContext;
+
+public class MongoDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    public MongoDbContext(DbContextOptions options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
