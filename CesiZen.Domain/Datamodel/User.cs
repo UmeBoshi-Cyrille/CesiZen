1f855321d04270341323985355bf7faaabd66e69
Cyrille Umehara <Umehara_Cyrille@outlook.com>
2025-01-07 22:12:52 +0100

    feat(Auth): prepared User model for MongoDb

--- CesiZen.Domain/Datamodel/User.cs
+++ CesiZen.Domain/Datamodel/User.cs
@@ -1,14 +1,52 @@
 ﻿using CesiZen.Domain.Enum;
+using Microsoft.AspNetCore.Identity;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace CesiZen.Domain.Datamodel;
 
-public class User : AUser
+public class User : IdentityUser
 {
-    public string FirstName { get; set; } = string.Empty;
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
 
-    public string LastName { get; set; } = string.Empty;
+    [BsonElement("firstName")]
+    public string FirstName { get; set; }
 
-    public Login Login { get; set; } = new();
+    [BsonElement("lastName")]
+    public string LastName { get; set; }
 
+    [BsonElement("username")]
+    public string Username { get; set; }
+
+    [BsonElement("email")]
+    public string Email { get; set; }
+
+    [BsonElement("passwordHash")]
+    public string PasswordHash { get; set; }
+
+    [BsonElement("salt")]
+    public string Salt { get; set; }
+
+    [BsonElement("createdAt")]
+    public DateTime CreatedAt { get; set; }
+
+    [BsonElement("lastLoginAt")]
+    public DateTime? LastLoginAt { get; set; }
+
+    [BsonElement("updatedAt")]
+    public DateTime? UpdatedAt { get; set; }
+
+    [BsonElement("isActive")]
+    public bool IsActive { get; set; }
+
+    [BsonElement("role")]
     public RoleType Role { get; set; }
+
+    [BsonElement("refreshToken")]
+    public string RefreshToken { get; set; }
+
+    [BsonElement("refreshTokenExpiryTime")]
+    public DateTime? RefreshTokenExpiryTime { get; set; }
 }
