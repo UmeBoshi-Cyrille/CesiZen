07759a993de8bf41070a104d805c4435e39508a3
Cyrille Umehara <Umehara_Cyrille@outlook.com>
2025-02-26 23:58:48 +0100

    Controllers

---
+++ CesiZen/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CesiZen.Api.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class TestController : ControllerBase
+{
+    [HttpGet("Test")]
+    public IActionResult Test()
+    {
+        return Ok("Got it");
+    }
+
+    [HttpGet("AuthorizedTest")]
+    [Authorize]
+    public IActionResult AuthorizedTest()
+    {
+        return Ok("Got it");
+    }
+}
