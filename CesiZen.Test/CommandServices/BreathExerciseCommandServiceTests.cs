61f2b5c9e5681290d4a0aa115559f8e3a9019eaf
Cyrille UMEHARA <cumehara@partenordhabitat.fr>
2025-03-19 14:31:10 +0100

    fix(Test): fixed Breath exercises test.

--- CesiZen.Test/CommandServices/BreathExerciseCommandServiceTests.cs
+++ CesiZen.Test/CommandServices/BreathExerciseCommandServiceTests.cs
@@ -5,6 +5,8 @@ using CesiZen.Domain.Interfaces;
 using CesiZen.Domain.Mapper;
 using CesiZen.Infrastructure.DatabaseContext;
 using CesiZen.Test.Fakers;
+using CesiZen.Test.Utils;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using Serilog;
 
@@ -16,12 +18,17 @@ public class BreathExerciseCommandServiceTests
     private readonly Mock<IBreathExerciseCommand> mockCommand;
     private readonly BreathExerciseCommandService service;
     private Mock<MongoDbContext> mockContext;
+    private Mock<DbSet<BreathExercise>> mockSet;
 
     public BreathExerciseCommandServiceTests()
     {
+        var options = new DbContextOptionsBuilder<MongoDbContext>()
+            .UseMongoDB("mongodb://localhost:27017", "TestDB")
+            .Options;
+
         loggerMock = new Mock<ILogger>();
         mockCommand = new Mock<IBreathExerciseCommand>();
-        mockContext = new Mock<MongoDbContext>();
+        mockContext = new Mock<MongoDbContext>(options);
         service = new BreathExerciseCommandService(loggerMock.Object, mockCommand.Object);
     }
 
@@ -29,19 +36,20 @@ public class BreathExerciseCommandServiceTests
     public async Task Insert_Success_ReturnsSuccessResult()
     {
         // Arrange
-        var dto = BreathExerciseFaker.FakeBreathExerciseDtoGenerator().Generate();
-        var entity = dto.Map();
+        var dtos = BreathExerciseFaker.FakeBreathExerciseDtoGenerator().Generate(10);
+        var entities = dtos.Map();
+        mockSet = CommonFaker.CreateMockDbSet(entities);
+        mockContext.Setup(c => c.BreathExercises).Returns(mockSet.Object);
         mockCommand.Setup(c => c.Insert(It.IsAny<BreathExercise>()))
             .ReturnsAsync(Result.Success());
 
         // Act
-        var result = await service.Insert(dto);
+        var result = await service.Insert(dtos[0]);
 
         // Assert
         Assert.True(result.IsSuccess);
-        mockCommand.Verify(c => c.Insert(It.Is<BreathExercise>(e => e == entity)), Times.Once);
-        mockContext.Verify(c => c.BreathExercises.Any());
-        mockContext.Verify(c => c.BreathExercises.FirstOrDefault().Title == entity.Title);
+        mockCommand.Verify(c => c.Insert(It.Is<BreathExercise>(e => e.Title == entities[0].Title)), Times.Once);
+        Assert.True(mockContext.Object.BreathExercises.FirstOrDefault().Title == entities[0].Title);
     }
 
     [Fact]
@@ -64,19 +72,19 @@ public class BreathExerciseCommandServiceTests
     public async Task Update_Success_ReturnsSuccessResult()
     {
         // Arrange
-        var dto = BreathExerciseFaker.FakeBreathExerciseDtoGenerator().Generate();
-        var entity = dto.Map();
-        mockContext.Setup(c => c.BreathExercises.Add(entity));
-        mockCommand.Setup(c => c.Update(It.IsAny<BreathExercise>()))
-            .ReturnsAsync(Result.Success());
+        var dtos = BreathExerciseFaker.FakeBreathExerciseDtoGenerator().Generate(10);
+        var entities = dtos.Map();
+        MockSetter(entities, CommandSelector.C1);
 
         // Act
-        var result = await service.Update(dto);
+        var result = await service.Update(dtos[0]);
 
         // Assert
         Assert.True(result.IsSuccess);
-        mockCommand.Verify(c => c.Update(It.Is<BreathExercise>(e => e == entity)), Times.Once);
-        mockContext.Verify(c => c.BreathExercises.FirstOrDefault().Title == dto.Title);
+        mockCommand.Verify(c => c.Update(
+            It.Is<BreathExercise>(e => e.Id == dtos[0].Id && e.Title == dtos[0].Title)), Times.Once);
+        Assert.True(mockContext.Object.BreathExercises
+            .Any(e => e.Title == dtos[0].Title && e.Id == dtos[0].Id));
     }
 
     [Fact]
@@ -99,19 +107,15 @@ public class BreathExerciseCommandServiceTests
     public async Task Delete_Success_ReturnsSuccessResult()
     {
         // Arrange
-        string id = "1";
-        var entity = BreathExerciseFaker.FakeBreathExerciseGenerator().Generate();
-        mockContext.Setup(c => c.BreathExercises.Add(entity));
-        mockCommand.Setup(c => c.Delete(id))
-            .ReturnsAsync(Result.Success());
+        var entities = BreathExerciseFaker.FakeBreathExerciseGenerator().Generate(10);
+        MockSetter(entities, CommandSelector.C2);
 
         // Act
-        var result = await service.Delete(id);
+        var result = await service.Delete(entities[0].Id);
 
         // Assert
         Assert.True(result.IsSuccess);
-        mockCommand.Verify(c => c.Delete(id), Times.Once);
-        mockContext.Verify(c => !c.Articles.Any());
+        mockCommand.Verify(c => c.Delete(It.IsAny<string>()), Times.Once);
     }
 
     [Fact]
@@ -131,5 +135,43 @@ public class BreathExerciseCommandServiceTests
         // Assert
         Assert.True(result.IsFailure);
         loggerMock.Verify(l => l.Error(It.IsAny<string>()), Times.Once);
+    }
+
+    private void MockSetter(List<BreathExercise> entities, CommandSelector commandSelector)
+    {
+        mockSet = CommonFaker.CreateMockDbSet(entities);
+        mockContext.Setup(c => c.BreathExercises).Returns(mockSet.Object);
+        MockCommandSelector(entities, commandSelector);
+    }
+
+    private void MockCommandSelector(List<BreathExercise> articles, CommandSelector commandSelector)
+    {
+        switch (commandSelector)
+        {
+            case CommandSelector.C1:
+                mockCommand.Setup(c => c.Update(It.IsAny<BreathExercise>())).Callback<BreathExercise>(
+                    updatedArticle =>
+                    {
+                        var article = articles.FirstOrDefault(a => a.Id == updatedArticle.Id);
+                        if (article != null)
+                        {
+                            article.Title = updatedArticle.Title;
+                        }
+                    }
+                ).ReturnsAsync(Result.Success());
+                break;
+            case CommandSelector.C2:
+                mockCommand.Setup(c => c.Delete(It.IsAny<string>())).Callback<string>(
+                    id =>
+                    {
+                        var article = articles.FirstOrDefault(a => a.Id == id);
+                        if (article != null)
+                        {
+                            articles.Remove(article);
+                        }
+                    }
+                ).ReturnsAsync(Result.Success());
+                break;
+        }
     }
 }
