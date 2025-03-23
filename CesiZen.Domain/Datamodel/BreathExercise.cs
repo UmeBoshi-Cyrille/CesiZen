727526fabf99bb92b32f4b51c23cecf4e8a02ce8
Cyrille Umehara <Umehara_Cyrille@outlook.com>
2025-03-23 22:27:40 +0100

    fix(Migration): removed

--- CesiZen.Domain/Datamodel/BreathExercise.cs
+++ CesiZen.Domain/Datamodel/BreathExercise.cs
@@ -1,11 +1,9 @@
 ﻿using CesiZen.Domain.Enum;
 using Newtonsoft.Json;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CesiZen.Domain.Datamodel;
 
-[NotMapped]
 public class BreathExercise
 {
     public int Id { get; set; }
