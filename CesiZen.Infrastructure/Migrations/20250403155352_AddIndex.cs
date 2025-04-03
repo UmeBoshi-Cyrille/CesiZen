604ed8927b317087b506057d30aa955859a0515a
Cyrille Umehara <Umehara_Cyrille@outlook.com>
2025-04-03 19:12:13 +0200

    feat(migration): index added to article

---
+++ CesiZen.Infrastructure/Migrations/20250403155352_AddIndex.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CesiZen.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenExpiryTime",
+                table: "Users");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Articles_CreatedAt",
+                table: "Articles",
+                column: "CreatedAt");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Articles_Title",
+                table: "Articles",
+                column: "Title");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Articles_CreatedAt",
+                table: "Articles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Articles_Title",
+                table: "Articles");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefreshTokenExpiryTime",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+    }
+}
