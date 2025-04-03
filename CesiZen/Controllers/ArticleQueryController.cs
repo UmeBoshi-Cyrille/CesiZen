a87b8cc733cbc00272d25dcd2fc949a5f22cde67
Cyrille UMEHARA <cumehara@partenordhabitat.fr>
2025-04-03 10:50:32 +0200

    feat(controller): Getby category method added to controller

--- CesiZen/Controllers/ArticleQueryController.cs
+++ CesiZen/Controllers/ArticleQueryController.cs
@@ -111,4 +111,32 @@ public class ArticleQueryController : ControllerBase
             failure: error => NotFound(new { message = error.Message })
         );
     }
+
+    /// <summary>
+    /// Retrieves a paginated list of articles filtered by the specified category.
+    /// </summary>
+    /// <param name="categoryId">The unique identifier of the desired category.</param>
+    /// <param name="pageNumber">The page number to retrieve, starting from 1.</param>
+    /// <param name="pageSize">The number of articles to include per page.</param>
+    /// <response code="200">The data was successfully retrieved.</response>
+    /// <response code="404">No articles were found for the specified category.</response>
+    /// <response code="500">An unexpected error occurred while processing the request.</response>
+    /// <returns>
+    /// A paginated result containing articles belonging to the specified category.
+    /// Returns a 200 status code if data is successfully retrieved.
+    /// Returns a 404 status code if no articles are found for the given category.
+    /// Returns a 500 status code if an internal server error occurs.
+    /// </returns>
+    [HttpGet("get-by-category")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult<PagedResultDto<ArticleMinimumDto>>> GetByCategory(int categoryId, int pageNumber = 1, int pageSize = 10)
+    {
+        var result = await articleService.GetByCategory(categoryId, pageNumber, pageSize);
+        return result.Match<ActionResult, PagedResultDto<ArticleMinimumDto>>(
+            success: value => Ok(new { value }),
+            failure: error => NotFound(new { message = error.Message })
+        );
+    }
 }
