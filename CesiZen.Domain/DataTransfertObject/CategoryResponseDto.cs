a9c1939e350ee21c05a6472d15ace09246392b80
Cyrille <98464975+UmeBoshi-Cyrille@users.noreply.github.com>
2025-04-07 21:16:46 +0200

    feat(model): imagePath added to category to add image (#27)

--- CesiZen.Domain/DataTransfertObject/CategoryResponseDto.cs
+++ CesiZen.Domain/DataTransfertObject/CategoryResponseDto.cs
@@ -1,11 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace CesiZen.Domain.DataTransfertObject;
+﻿namespace CesiZen.Domain.DataTransfertObject;
 
 public class CategoryResponseDto
 {
     public int Id { get; set; }
-
-    [MaxLength(54)]
     public string Name { get; set; } = string.Empty;
+    public string ImagePath { get; set; } = string.Empty;
 }
---
+++ CesiZen.Infrastructure/Migrations/20250407165112_CategoryImage.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CesiZen.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CategoryImage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "Categories",
+                type: "character varying(54)",
+                maxLength: 54,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "Categories");
+        }
+    }
+}
