8419460b5b73816e481215f8733a78e06a5d37a9
Cyrille Umehara <Umehara_Cyrille@outlook.com>
2025-04-08 23:24:48 +0200

    feat(config): credentials allowed and managed by aspnet

--- CesiZen/Program.cs
+++ CesiZen/Program.cs
@@ -45,8 +45,8 @@ app.UseSerilogRequestLogging();
 
 app.UseHttpsRedirection();
 
-app.UseAuthentication();
 app.UseCors("AllowAngularClient");
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
