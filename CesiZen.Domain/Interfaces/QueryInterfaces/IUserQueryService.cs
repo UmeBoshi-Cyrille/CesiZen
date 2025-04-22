a192b916ed7fde2847c8b848d8550040a42da493
Cyrille <98464975+UmeBoshi-Cyrille@users.noreply.github.com>
2025-04-23 00:22:34 +0200

    409 enhancement (#46)
    * fix(service): returjn Userdto instead of minimum dto that is for index
    
    * fix(service): add loginservice to register service
    - changed http method to patch
    - context users to count users

--- CesiZen.Domain/Interfaces/QueryInterfaces/IUserQueryService.cs
+++ CesiZen.Domain/Interfaces/QueryInterfaces/IUserQueryService.cs
@@ -2,8 +2,12 @@
 
 namespace CesiZen.Domain.Interfaces;
 
-public interface IUserQueryService : IQueryServiceInterface<UserMinimumDto>
+public interface IUserQueryService
 {
     Task<IResult<PagedResultDto<UserMinimumDto>>> SearchUsers(PageParametersDto parameters, string searchTerm);
     Task<IResult<UserDto>> GetByUsername(string username);
+
+    Task<IResult<UserDto>> GetByIdAsync(int id);
+
+    Task<IResult<PagedResultDto<UserMinimumDto>>> GetAllAsync(int pageNumber, int pageSize);
 }
