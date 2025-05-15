e97aa3fea3be23580158afe84487e93624985e74
Umeboshi <umehara_cyrille@outlook.com>
2025-05-15 07:43:43 +0200

    fix(dto): lenght and regex adapted

--- CesiZen.Domain/DataTransfertObject/NewDto/NewArticleDto.cs
+++ CesiZen.Domain/DataTransfertObject/NewDto/NewArticleDto.cs
@@ -5,21 +5,21 @@ namespace CesiZen.Domain.DataTransfertObject;
 
 public class NewArticleDto
 {
-    [MaxLength(54, ErrorMessage = "54 caractères autorisés")]
+    [MaxLength(255, ErrorMessage = "255 caractères autorisés")]
     [MinLength(2, ErrorMessage = "2 caractères minimum requis")]
-    [RegularExpression(@"^\S[a-zA-Z !^()_;':"",.?\p{L}+]+\S$", ErrorMessage = "Caractères présents non autorisés.")]
+    [RegularExpression(@"^\S[a-zA-ZÀ-ÿ\s !^()-_;'’:"",.?\p{L}+]+\S$", ErrorMessage = "Caractères présents non autorisés.")]
     [DefaultValue("Titre")]
     [Required(ErrorMessage = "Le titre est obligatoire")]
     public string Title { get; set; } = string.Empty;
 
     [MaxLength(3000, ErrorMessage = "3000 caractères autorisés")]
-    [RegularExpression(@"^\S[a-zA-Z !^()_;':"",.?\p{L}+]+\S$", ErrorMessage = "Caractères présents non autorisés.")]
+    [RegularExpression(@"^\S[a-zA-ZÀ-ÿ\s !^()%-_;'’:"",.?\p{L}+]+$", ErrorMessage = "Caractères présents non autorisés.")]
     [Required(ErrorMessage = "La description est obligatoire")]
     public string Description { get; set; } = string.Empty;
 
     [MaxLength(54, ErrorMessage = "54 caractères autorisés")]
     [MinLength(2, ErrorMessage = "2 caractères minimum requis")]
-    [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Caractères présents non autorisés")]
+    [RegularExpression(@"^[a-zA-ZÀ-ÿ]+$", ErrorMessage = "Caractères présents non autorisés")]
     [Required(ErrorMessage = "Le champs auteur est obligatoire.")]
     [DefaultValue("Auteur")]
     public string Author { get; set; } = string.Empty;
--- CesiZen.Domain/DataTransfertObject/NewDto/NewCategoryDto.cs
+++ CesiZen.Domain/DataTransfertObject/NewDto/NewCategoryDto.cs
@@ -6,7 +6,7 @@ public class NewCategoryDto
 {
     [MaxLength(54, ErrorMessage = "54 caractères autorisés")]
     [Required(ErrorMessage = "Le champs nom est obligatoire")]
-    [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Caractères présents non autorisés")]
+    [RegularExpression(@"^[a-zA-ZÀ-ÿ-]+$", ErrorMessage = "Caractères présents non autorisés")]
     public string Name { get; set; } = string.Empty;
 
     [MaxLength(54)]
